6af802ef8803909f84a7aea6d7f4f9f67319adcc
Álvaro Corrochano López <alvaroybroly@gmail.com>
2021-06-17 16:02:22 +0200

    Disparo Vertical OK

--- Practica Final - Metal Slug/src/Rossi.js
+++ Practica Final - Metal Slug/src/Rossi.js
@@ -374,6 +374,7 @@ function add_Rossi(Q) {
 				projectileSpeed: 500,
 				mg: false,
 				onShoot: false,
+				vS: false,
 			});
 			this.add("animation, tween, platformerControls");
 			Q.input.on("fire", this, "attackAction");
@@ -402,13 +403,25 @@ function add_Rossi(Q) {
 					
 
 					if(legs.lookback) {
-						this.p.y = legs.p.y;
-						this.p.x = legs.p.x-3;
+						if(!this.p.vS){
+							this.p.y = legs.p.y;
+							this.p.x = legs.p.x-3;
+						}
+						else{
+							this.p.y = legs.p.y-20;
+							this.p.x = legs.p.x-3;
+						}
 						this.play("chest_stand_left")
 					}
 					else {
-						this.p.y = legs.p.y;
-						this.p.x = legs.p.x+3;
+						if(!this.p.vS){
+							this.p.y = legs.p.y;
+							this.p.x = legs.p.x+3;
+						}
+						else{
+							this.p.y = legs.p.y-20;
+							this.p.x = legs.p.x+3;
+						}
 						this.play("chest_stand_right")
 					}
 				}
@@ -419,19 +432,32 @@ function add_Rossi(Q) {
 					}
 
 					if(legs.lookback) {
-						this.p.y = legs.p.y;
-						this.p.x = legs.p.x-5;
+						if(!this.p.vS){
+							this.p.y = legs.p.y;
+							this.p.x = legs.p.x-5;
+						}
+						else{
+							this.p.y = legs.p.y-25;
+							this.p.x = legs.p.x;
+						}
 						this.play("chest_stand_mg_left")
 					}
 					else {
-						this.p.y = legs.p.y;
-						this.p.x = legs.p.x+5;
+						if(!this.p.vS){
+							this.p.y = legs.p.y;
+							this.p.x = legs.p.x+5;
+						}
+						else{
+							this.p.y = legs.p.y-25;
+							this.p.x = legs.p.x;
+						}
 						this.play("chest_stand_mg_right")
 					}
 				}
 			}
 		},
 		attackAction: function(){
+			if(this.p.onShoot) return;
 
 			if(!this.p.mg){
 				this.p.sheet = "disparo";
@@ -475,7 +501,8 @@ function add_Rossi(Q) {
 		},
 
 		attackUpAction: function(){
-
+			if(this.p.onShoot) return;
+			
 			if(!this.p.mg){
 				this.p.sheet = "arriba";
 				this.size(true);
@@ -515,6 +542,7 @@ function add_Rossi(Q) {
 			}
 
 			this.p.onShoot = true;
+			this.p.vS = true;
 		},
 
 		gunShooting: function(){
@@ -550,12 +578,12 @@ function add_Rossi(Q) {
 			legs = legs.items[0];
 			
 			if (!legs.lookback){
-				offset = legs.p.w;
-				speed = this.p.projectileSpeed;
+				offset = legs.p.h * -1;
+				speed = -this.p.projectileSpeed;
 				this.play("after_chest_shoot_gun_up_right",200)
 			}
 			else{
-				offset = legs.p.w * -1;
+				offset = legs.p.h * -1;
 				speed = -this.p.projectileSpeed;
 				this.play("after_chest_shoot_gun_up_left",200)
 			}
@@ -607,12 +635,12 @@ function add_Rossi(Q) {
 			legs = legs.items[0];
 			
 			if (!legs.lookback){
-				offset = legs.p.w;
-				speed = this.p.projectileSpeed;
+				offset = legs.p.h * -1;
+				speed = -this.p.projectileSpeed;
 				this.play("after_chest_shoot_mg_up_right",200)
 			}
 			else{
-				offset = legs.p.w * -1;
+				offset = legs.p.h * -1;
 				speed = -this.p.projectileSpeed;
 				this.play("after_chest_shoot_mg_up_left",200)
 			}
@@ -629,6 +657,7 @@ function add_Rossi(Q) {
 
 		shootStop: function(){
 			this.p.onShoot = false;
+			this.p.vS = false;
 		},
 
 		getMachineGun: function(){
